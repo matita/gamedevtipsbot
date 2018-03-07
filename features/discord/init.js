da5a28e9a3891765eab666c189856c8fd1ec642e
Glitch (gamedevtipsbot) <none>
2018-03-07 22:52:23 +0000

    🏗😂 Checkpoint features/discord/init.js:657646/623

--- features/discord/init.js
+++ features/discord/init.js
@@ -1,4 +1,5 @@
 const serversDb = require('../../models/servers')
+const tipsDb = require('../../models/tips')
 
 module.exports = client => {
   serversDb
@@ -7,8 +8,25 @@ module.exports = client => {
       if (err)
         return console.error('Error while getting registered servers', err)
     
-      servers.forEach(s => {
-        
-      })
+      let count = 5
+      const i = setInterval(() => {
+        servers.forEach(s => {
+          const channelId = s.defaultChannelId
+          if (!channelId)
+            return
+          const channel = client.channels.get(channelId)
+          if (!channel)
+            return
+          
+          tipsDb.find({}).exec((err, allTips) => {
+            if (err)
+              return console.error('Error while searching for all tips')
+            
+            const index = Math.floor(Math.random() * allTips.length)
+            const tip = allTips[index]
+            
+          })
+        })
+      }, 10000)
     })
 }
