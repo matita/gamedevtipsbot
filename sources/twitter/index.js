f4e0a51764c219b0917d7702738f1e4a14ad17cb
Glitch (gamedevtipsbot) <none>
2018-03-07 11:30:30 +0000

    🎡🏁 Checkpoint models/tips.js:657646/25 sources/twitter/index.js:657646/77 .env:657646/1

---
+++ sources/twitter/index.js
@@ -0,0 +1,7 @@
+const Twitter = require('twit')
+const twit = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
