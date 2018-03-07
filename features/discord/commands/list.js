d4358fd1e535543a0d4d7fbe3a95968c7b64835a
Glitch (gamedevtipsbot) <none>
2018-03-07 15:01:14 +0000

    🌺🚃 Checkpoint features/discord/commands/list.js:657646/322

--- features/discord/commands/list.js
+++ features/discord/commands/list.js
@@ -5,6 +5,11 @@ module.exports = (message, text) => {
     .sort({ createdAt: 1 })
     .limit(10)
     .exec((err, foundTips) => {
-      if
+      if (err)
+        return message.reply('Error\n```\n' + err.message + '\n```')
+    
+      const rows = ['Here the latest 10 tips']
+        .concat(foundTips.map((t, i) => (i + 1) + '. ' + t.text))
+      message.channel.send(rows.join('\n'))
     });
 }
