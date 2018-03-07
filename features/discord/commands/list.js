5a46146593a84723fdba72187c297c6fabd53456
Glitch (gamedevtipsbot) <none>
2018-03-07 15:22:29 +0000

    🌈😨 Checkpoint features/discord/commands/list.js:657646/288

--- features/discord/commands/list.js
+++ features/discord/commands/list.js
@@ -1,15 +1,18 @@
 const tips = require('../../../models/tips')
 
 module.exports = (message, text) => {
+  const countMatch = text.match(/\b(\d+)\b/)
+  const count = countMatch ? countMatch[1] : 5
+  
   tips.find({})
     .sort({ createdAt: 1 })
-    .limit(10)
+    .limit(count)
     .exec((err, foundTips) => {
       if (err)
         return message.reply('Error\n```\n' + err.message + '\n```')
     
-      const rows = ['Here the latest 10 tips']
+      const rows = [`Here the latest ${count} tips`]
         .concat(foundTips.map((t, i) => (i + 1) + '. ' + t.text))
-      message.channel.send(rows.join('\n'))
+      message.channel.send(rows.join('\n\n'))
     });
 }
