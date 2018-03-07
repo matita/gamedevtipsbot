3fe1b5c6df4ef895a14190815a5d66a18a77c8ea
Glitch (gamedevtipsbot) <none>
2018-03-07 13:17:18 +0000

    🎯🚧 Checkpoint sources/twitter/users/tips4unity.js:657646/27

--- sources/twitter/users/tips4unity.js
+++ sources/twitter/users/tips4unity.js
@@ -1,6 +1,17 @@
 const userId = '2490064238'
 
-const init = twit => {}
+const init = twit => {
+  console.log('Retrieveing tweets for ' + userId)
+  twit.get('statuses/user_timeline', { user_id: userId, count: 200, tweet_mode: 'extended' }, (err, tweets, res) => {
+    if (err)
+      return console.error(err)
+
+    const episodes = tweets.map(toChallengeEpisode)
+    episodes.forEach(e => add('pixel_dailies', e))
+    
+    console.log('filtered tweets', filteredTweets.map(/*t => t.text + ' ' + new Date(t.created_at)*/toChallengeEpisode))
+  })
+}
 
 const receivedTweet = (tweet, twit) => {}
 
