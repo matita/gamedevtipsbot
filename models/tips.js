ca100e0a50d6f4404760b6b8ef8646c11e4aa068
Glitch (gamedevtipsbot) <none>
2018-03-07 13:58:56 +0000

    🌪🎢 Checkpoint models/tips.js:657646/293

--- models/tips.js
+++ models/tips.js
@@ -4,13 +4,4 @@ const tips = new Datastore({
   timestampData: true,
   autoload: true})
 
-//tips.find({}, (err, foundTips) => { if (err) console.error(err); console.log(foundTips) })
-
-tips.remove({ source: { $exists: false } }, { multi: true }, (err, numRemoved) => {
-  if (err)
-    return console.error('error while removing tips without source', err)
-  
-  console.log('removed', numRemoved, 'tips without source')
-})
-
 module.exports = tips
