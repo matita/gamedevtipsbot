ad324dd334174ebb4b7028d14a9a5c631ab7a6c6
Glitch (gamedevtipsbot) <none>
2018-03-07 22:20:47 +0000

    😋🎤 Checkpoint models/server.js:657646/72 features/discord/commands/channel.js:657646/520

---
+++ features/discord/commands/channel.js
@@ -0,0 +1,13 @@
+const { getServer } = require('../../../models/server')
+
+module.exports = (message, text) => {
+  try {
+    const server = getServer(message.channel.guild.id)
+    const channelId = server.defaultChannelId
+    if (!channelId)
+      return message.channel.send('No default channel set')
+    message.channel.send(`Default channel is <#${channelId}>`)
+  } catch (err) {
+    message.channel.send('```\n' + err + '\n```')
+  }
+}
