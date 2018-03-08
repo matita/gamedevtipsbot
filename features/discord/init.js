f8a833e7a6380d7e0f2eb049d1fc56ab44cf7f81
Glitch (gamedevtipsbot) <none>
2018-03-08 09:29:50 +0000

    🐯🎊 Checkpoint features/discord/init.js:657646/417

--- features/discord/init.js
+++ features/discord/init.js
@@ -1,30 +1,27 @@
+const schedule = require('node-schedule')
 const serversDb = require('../../models/servers')
 const randomTip = require('./utils/randomTip')
 
-module.exports = client => {
-  return;
+const sendTips = () => {
   serversDb
     .find({ defaultChannelId: { $exists: true } })
     .exec((err, servers) => {
       if (err)
         return console.error('Error while getting registered servers', err)
     
-      let count = 5
-      const i = setInterval(() => {
-        if (count <= 0)
-          return 
-        count--
+      servers.forEach(s => {
+        const channelId = s.defaultChannelId
+        if (!channelId)
+          return
+        const channel = client.channels.get(channelId)
+        if (!channel)
+          return
           
-        servers.forEach(s => {
-          const channelId = s.defaultChannelId
-          if (!channelId)
-            return
-          const channel = client.channels.get(channelId)
-          if (!channel)
-            return
-          
-          randomTip(channel)
-        })
-      }, 10000)
+        randomTip(channel)
+      })
     })
+}
+
+module.exports = client => {
+  const job = schedule.scheduleJob('30 * * * *', sendTips)
 }
