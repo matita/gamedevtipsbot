72fceb2e6bcbd57a17b3590d429c993dc769a955
Glitch (gamedevtipsbot) <none>
2018-03-08 10:31:53 +0000

    👘🎃 Checkpoint models/channel.js:657646/1061

--- models/channel.js
+++ models/channel.js
@@ -1,7 +1,36 @@
 const channelsDb = require('./channels')
+const tipsDb = require('./tips')
 
 const factory = channel => ({
   ...channel,
+  
+  sentTipsIds: channel.sentTipsIds || [],
+  
+  getRandomUnsentTip: () => {
+    return new Promise((resolve, reject) => {
+      tipsDb.find({ _id: { $nin: channel.sentTipsIds }}).exec((err, unsentTips) => {
+        if (err)
+          return reject(err)
+        
+        const index = Math.floor(Math.random() * unsentTips.length)
+        const tip = unsentTips[index]
+        resolve({ tip, remaining: unsentTips.length - 1 })
+      })
+    })
+  },
+  
+  sendTip: tip => {
+    return new Promise((resolve, reject) => {
+      if (channel.sentTipsIds.indexOf(tip._id) !== -1)
+        return resolve(factory(channel));
+
+      channel.sentTipsIds.push(tip._id)
+      channelsDb.update({ _id: channel._id }, channel).exec((err, c) => {
+        if (err) reject(err)
+        else resolve(factory(c))
+      })
+    })
+  }
 })
 
 const getChannel = channelId => new Promise((resolve, reject) => {
