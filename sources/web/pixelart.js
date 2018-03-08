b627cda133abae802d06a10a4b2b1e5eb7608616
Glitch (gamedevtipsbot) <none>
2018-03-08 17:32:00 +0000

    🏦👽 Checkpoint sources/web/pixelart.js:657646/272

--- sources/web/pixelart.js
+++ sources/web/pixelart.js
@@ -1,16 +1,25 @@
 // http://blog.studiominiboss.com/pixelart
 const axios = require('axios')
 const cheerio = require('cheerio')
+const addTip = 
 
 const url = 'http://blog.studiominiboss.com/pixelart'
 
 const getTips = html => {
   const $ = cheerio.load(html)
-  const container = $('#content article .copy')
+  const container = $('#content article')
   
   let i = 0
-  container.find('p').each(() => {
-    const $p = $(this)
+  const tips = []
+  let currentTip
+  
+  container.find('p').each((i, p) => {
+    const $p = $(p)
+    const text = $p.text().trim()
+    if (text.match(/^#\s*\d+/)) {
+      currentTip = {}
+      tips.push(currentTip)
+    }
     
     console.log((i++) + '. ' + $p.text())
   })
