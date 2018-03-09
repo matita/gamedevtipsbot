b2a30a1775efcc8c71060e628e98cc0ea4e1aba6
Glitch (gamedevtipsbot) <none>
2018-03-09 17:31:18 +0000

    🍡🌭 Checkpoint features/discord/commands/setchannel.js:657646/2 features/discord/commands/channel.js:657646/466

--- features/discord/commands/channel.js
+++ features/discord/commands/channel.js
@@ -1,11 +1,21 @@
 const { getServer } = require('../../../models/server')
+const extractChannels = require('../utils/extractChannels')
 
 module.exports = async (message, text, server) => {
   try {
-    const channelId = server.defaultChannelId
+    const { newText, channelsIds } = extractChannels(text)
+    const channelId = channelsIds[0]
     if (!channelId)
-      return message.channel.send('No default channel set')
-    message.channel.send(`Default channel is <#${channelId}>`)
+      return message.channel.send('You have to specify a channel like this:\n```\nchannel #channel-name [space-separated tags]\n```')
+    
+    const tags = newText.toLowerCase().split(/\s+/)
+      .filter(t => !!t)
+
+    const channel = await server.getChannel(channelId)
+    await channel.setTags(tags)
+    message.channel.send('I\'ll send tips' + 
+                         (tags.length ? ' with tags ' + tags.map(t => `*${t}*`).join(', ') : '') +
+                         ` to <#${channelId}> every day`)
   } catch (err) {
     message.channel.send('```\n' + err + '\n```')
   }
