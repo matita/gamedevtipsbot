3683157b362cf0251a9e1ecd25a6a1cc8869837e
Glitch (gamedevtipsbot) <none>
2018-03-09 21:40:53 +0000

    🍘🎽 Checkpoint features/discord/commands/remove.js:657646/307

--- features/discord/commands/remove.js
+++ features/discord/commands/remove.js
@@ -1,11 +1,16 @@
 const extractChannels = require('../utils/extractChannels')
 
-module.exports = (message, text, server) => {
-  const { newText, channelsIds } = extractChannels(text)
-  if (!channelsIds.length)
-    return message.channel.send('You have to specify the channels to remove, like this:\n```\n' +
-                               '@' + message.client.username + ' remove #channel-name #second.channel-name' +
-                               '\n```')
-  
-  
+module.exports = async (message, text, server) => {
+  try {
+    const { newText, channelsIds } = extractChannels(text)
+    if (!channelsIds.length)
+      return message.channel.send('You have to specify the channels to remove, like this:\n```\n' +
+                                 '@' + message.client.username + ' remove #channel-name #second.channel-name' +
+                                 '\n```')
+    
+    const numRemoved = await server.removeChannels(channelsIds)
+    await message.channel.send('Removed ' + numRemoved + ' channels')
+  } catch (err) {
+    message.channel.send('```\n' + err.message + '\n```')
+  }
 }
