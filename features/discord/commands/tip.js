690001fab9e7b712d67dcd25a0aff4f0963e2e4a
Glitch (gamedevtipsbot) <none>
2018-03-09 13:14:32 +0000

    🏍👰 Checkpoint features/discord/commands/tip.js:657646/64

--- features/discord/commands/tip.js
+++ features/discord/commands/tip.js
@@ -2,8 +2,10 @@ const { getChannel } = require('../../../models/channel')
 const randomTip = require('../utils/randomTip')
 
 module.exports = async (message, text) => {
-  const discordChannel = message.channel
   const tags = text.toLowerCase().trim().split(/\s+/)
+    .filter(t => !!t) // filter out empty strings
+  
+  const discordChannel = message.channel
   
   try {
     const channel = await getChannel(discordChannel.id)
