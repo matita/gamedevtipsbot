dc62c1866c1e61d7c36413f2ab71b8cf3f1fb027
Glitch (gamedevtipsbot) <none>
2018-03-09 16:15:15 +0000

    💕⛄️ Checkpoint features/discord/utils/tipToEmbed.js:657646/1

--- features/discord/utils/tipToEmbed.js
+++ features/discord/utils/tipToEmbed.js
@@ -1,6 +1,7 @@
 module.exports = tip => ({
   title: tip.source,
   description: tip.text,
+  url: tip.url,
   //image: { url: 'http://78.media.tumblr.com/6717e8d14a5e39f02c7933835f9af992/tumblr_inline_p4ilt5P9Vl1qdiwz3_1280.gif' },
   image: tip.imageUrl ? { url: tip.imageUrl } : null,
   footer: { text: tip.tags.map(t => '#' + t).join(' ') }
