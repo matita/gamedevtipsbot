12e77ab37f9a05799f32b4c72b9a258bd0034d42
Glitch (gamedevtipsbot) <none>
2018-03-09 22:36:53 +0000

    🏐🎱 Checkpoint sources/web/unitytip.js:657646/172

--- sources/web/unitytip.js
+++ sources/web/unitytip.js
@@ -1,1 +1,9 @@
 // http://unitytip.com/
+
+const axios = require('axios')
+const cheerio = require('cheerio')
+const htmlToMarkdown = require('html-to-markdown')
+
+const url = 'http://unitytip.com/'
+
+const getTips =
