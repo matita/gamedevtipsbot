a6392b8df5b7464a1b5a4e5cfd204914a25800e4
Glitch (gamedevtipsbot) <none>
2018-03-09 10:45:29 +0000

    🍤🍙 Checkpoint sources/web/pixelart.js:657646/55 sources/patreon/utils/api.js:657646/179

---
+++ sources/patreon/utils/api.js
@@ -0,0 +1,9 @@
+const axios = require('axios')
+
+// https://www.patreon.com/api/stream?include=attachments%2Cuser_defined_tags&fields[post]=content%2Cembed%2Cimage%2Cpost_file%2Cpublished_at%2Cpatreon_url%2Cpost_type%2Cpledge_url%2Cthumbnail_url%2Ctitle%2Curl&fields[user]=image_url%2Cfull_name%2Curl&fields[post]=%2Ccontent%2Cembed%2Cimage%2Cpost_file%2Cpublished_at%2Cpatreon_url%2Cpost_type%2Cpledge_url%2Cthumbnail_url%2Ctitle%2Curl&fields[user]=image_url%2Cfull_name%2Curl&fields[campaign]=earnings_visibility&page[cursor]=2017-07-25T13%3A00%3A00Z&filter[is_by_creator]=true&filter[is_following]=false&filter[creator_id]=2279992&filter[user_defined_tags]=tutorial&filter[contains_exclusive_posts]=true&json-api-use-default-includes=false&json-api-version=1.0
+
+const baseUrl = 'https://www.patreon.com/api/stream'
+const defaultParams = {
+  include: 'attachments,user_defined_tags',
+  'fields[post]': 'content,embed,image,post_file,published_at,patreon_url,post_type%2Cpledge_url%2Cthumbnail_url%2Ctitle%2Curl&fields[user]=image_url%2Cfull_name%2Curl&fields[post]=%2Ccontent%2Cembed%2Cimage%2Cpost_file%2Cpublished_at%2Cpatreon_url%2Cpost_type%2Cpledge_url%2Cthumbnail_url%2Ctitle%2Curl&fields[user]=image_url%2Cfull_name%2Curl&fields[campaign]=earnings_visibility&page[cursor]=2017-07-25T13%3A00%3A00Z&filter[is_by_creator]=true&filter[is_following]=false&filter[creator_id]=2279992&filter[user_defined_tags]=tutorial&filter[contains_exclusive_posts]=true&json-api-use-default-includes=false&json-api-version=1.0
+}
