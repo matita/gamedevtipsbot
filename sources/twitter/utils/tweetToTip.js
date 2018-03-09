c7a6d5ce822a9fe5cdb87f7f1f35ee0509275373
Glitch (gamedevtipsbot) <none>
2018-03-09 16:37:15 +0000

    🍁💇 Checkpoint sources/twitter/utils/tweetToTip.js:657646/97 sources/twitter/users/tips4unity.js:657646/1

--- sources/twitter/utils/tweetToTip.js
+++ sources/twitter/utils/tweetToTip.js
@@ -1,7 +1,9 @@
 const replaceHtml = require('./replaceHtml')
+const tweetToLink = require('./tweetToLink')
 
 module.exports = ({ source, tags }) => (tweet) => ({
   _id: 'tw:' + tweet.id_str,
+  url: tweetToLink(tweet),
   source: source,
   text: replaceHtml(tweet.full_text),
   tags: tags
