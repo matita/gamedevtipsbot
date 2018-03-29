5872f8d5819520b3c8138020989af1372e60e48f
Glitch (gamedevtipsbot) <none>
2018-03-29 16:22:06 +0200

    removed auto fix on startup

--- index.js
+++ index.js
@@ -6,7 +6,3 @@ require('./features/discord')
 require('./sources/twitter')
 require('./sources/patreon/users/saint11')
 require('./sources/web/unitytip')
-
-// fix db images not in cdn
-require('./exec/upload-cdn')
-require('./exec/fix-saint11-images')
