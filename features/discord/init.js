ac28fb82864137144ed9d4cfe2bd5f04152f2771
Glitch (gamedevtipsbot) <none>
2018-06-11 23:21:32 +0200

    remember to join steam group daily

--- features/discord/init.js
+++ features/discord/init.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 const schedule = require('node-schedule')
 const serversDb = require('../../models/servers')
 const { serverFactory } = require('../../models/server')
@@ -15,8 +16,20 @@ const sendTips = (client) => {
   })
 }
 
+/** @param {Discord.Client} client */
+const sendJoinSteamGroup = (client) => {
+  const channelId = process.env.GENERAL_CHAT
+  /** @type {Discord.TextChannel} */
+  const channel = client.channels.get(channelId)
+  if (!channel)
+    return console.log('could not find channel')
+  channel.send('Remember to join our Steam gaming group. Here we organise gaming tournaments and events\nhttps://steamcommunity.com/groups/CGDTGG')
+}
+
 module.exports = client => {
   const job = schedule.scheduleJob('0 30 19 * * *', sendTips.bind(null, client))
   //const job = schedule.scheduleJob('*/10 * * * * *', sendTips.bind(null, client))
+  const sendSteamGroupJob = schedule.scheduleJob('0 0 16 * * *', sendJoinSteamGroup.bind(null, client))
+  //const sendSteamGroupJob = schedule.scheduleJob('*/10 * * * * *', sendJoinSteamGroup.bind(null, client))
   console.log('scheduled job ' + new Date())
 }
